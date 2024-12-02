e9cb29465c161a60f4e989034a3448747ff5a33f
alex <alexadkl@gmail.com>
2024-12-02 10:08:08 +0100

    code commenté

--- Etape2/script.js
+++ Etape2/script.js
@@ -1,4 +1,4 @@
-const button = document.getElementById("btn");
+const button = document.getElementById("btn"); //on récupère les éléments de la page html
 const cityname = document.getElementById("cityname");
 const date = document.getElementById("date");
 const weather = document.getElementById("weather");
@@ -6,25 +6,24 @@ const temperature = document.getElementById("temperature");
 const wind = document.getElementById("wind");
 const humidity = document.getElementById("humidity");
 
-const nameElement = document.createElement("div");
+const nameElement = document.createElement("div"); //on crée des nouvelles div à l'intérieur des éléments de la page
 const dateElement = document.createElement("div");
 const weatherElement = document.createElement("div");
 const temperatureElement = document.createElement("div");
 const windElement = document.createElement("div");
 const humidityElement = document.createElement("div");
 
-const fetchWeather = async () => {
-  let input = document.getElementById("inputbox").value;
+const fetchWeather = async () => { // on déclare la fonction qui récupère les données sur l'API
+  
+  let input = document.getElementById("inputbox").value; //on récupère la valeur de l'input (nom de ville entré par l'utilisateur)
 
-  let requestString = `http://api.weatherapi.com/v1/current.json?key=b0e73d7dbcd346fc826145808242711&q=${input}&aqi=no`;
+  let requestString = `http://api.weatherapi.com/v1/current.json?key=b0e73d7dbcd346fc826145808242711&q=${input}&aqi=no`; //on injecte l'input dans la requête API
 
-  let data = await fetch(requestString);
-  console.log(data);
-  let response = await data.json();
-  console.log(response);
+  let data = await fetch(requestString); //on lance l'appel à l'API
+  let response = await data.json(); //on transforme les données au format json
 
-  nameElement.innerText = `Today in ${response["location"]["name"]}`;
-  cityname.appendChild(nameElement);
+  nameElement.innerText = `Today in ${response["location"]["name"]}`; //on injecte les données récupérées dans les balises html
+  cityname.appendChild(nameElement); //on ajoute la nouvelle div à l'intérieur de la balise html prévue
 
   weatherElement.innerText = `${response["current"]["condition"]["text"]}`;
   weather.appendChild(weatherElement);
@@ -37,9 +36,6 @@ const fetchWeather = async () => {
 
   humidityElement.innerText = `${response["current"]["humidity"]} %`;
   humidity.appendChild(humidityElement);
-
-  console.log(`${response["current"]["condition"]["text"]}`);
-  console.log(`${response["location"]["name"]}`);
 };
 
-button.addEventListener("click", fetchWeather);
+button.addEventListener("click", fetchWeather); //on ajoute une addEventListener au bouton : au click, on lance la fonction fetchWeather
