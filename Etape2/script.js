24384696546ba84695bae5dfe9e4130e7bf7c227
alex <alexadkl@gmail.com>
2024-12-04 16:49:25 +0100

    Projet fini !

--- Etape2/script.js
+++ Etape2/script.js
@@ -1,4 +1,4 @@
-const button = document.getElementById("btn"); //on récupère les éléments de la page html
+const button = document.getElementById("btn");                //on récupère les éléments de la page html
 const cityname = document.getElementById("cityname");
 const lastUpdate = document.getElementById("lastupdate");
 const weather = document.getElementById("weather");
@@ -10,17 +10,12 @@ const background = document.getElementById("background");
 const temperatureIcon = document.getElementById("temperatureIcon");
 const forecastWeather = document.getElementById("forecastWeather");
 const forecastWeatherIcon = document.getElementById("forecastWeatherIcon");
-const forecastTemperatureMin = document.getElementById(
-  "forecastTemperatureMin"
-);
-const forecastTemperatureMax = document.getElementById(
-  "forecastTemperatureMax"
-);
-
+const forecastTemperatureMin = document.getElementById("forecastTemperatureMin");
+const forecastTemperatureMax = document.getElementById("forecastTemperatureMax");
 const timeContainer = document.getElementById("time-Container");
-const timeElement = document.createElement("div");
 
-const nameElement = document.createElement("div"); //on crée des nouvelles div à l'intérieur des éléments de la page
+
+const nameElement = document.createElement("div");          //on crée des nouvelles div à l'intérieur des éléments de la page
 const lastUpdateElement = document.createElement("div");
 const weatherElement = document.createElement("div");
 const temperatureElement = document.createElement("div");
@@ -33,32 +28,32 @@ const forecastWeatherElement = document.createElement("div");
 const forecastWeatherIconElement = document.createElement("div");
 const forecastTemperatureMinElement = document.createElement("div");
 const forecastTemperatureMaxElement = document.createElement("div");
+const timeElement = document.createElement("div");
 
-// On utilise var  pour pouvoir accéder à response.
+// On utilise var pour pouvoir accéder à response et conditionText
 var response;
 let conditionText;
 
-let hours;
+let hours;    //on déclare des variables pour afficher l'heure en temps réel
 let minutes;
 let seconds;
 
-const fetchWeather = async () => {
-  // on déclare la fonction qui récupère les données sur l'API
+const fetchWeather = async () => {           // on déclare la fonction qui récupère les données sur l'API
 
   let input = document.getElementById("inputbox").value; //on récupère la valeur de l'input (nom de ville entré par l'utilisateur)
-  if (!input) {
+  if (!input) {                                           //si l'input est undefined, on met Paris
     input = "Paris";
-    console.log(input);
   }
+
   let requestString = `http://api.weatherapi.com/v1/current.json?key=c9ea07bc963e4bf7849141302240412&q=${input}&aqi=no`; //on injecte l'input dans la requête API
 
-  let data = await fetch(requestString); //on lance l'appel à l'API
-  response = await data.json(); //on transforme les données au format json
+  let data = await fetch(requestString);    //on lance l'appel à l'API
+  response = await data.json();             //on transforme les données au format json
 
-  nameElement.innerText = `Today in ${response["location"]["name"]}`; //on injecte les données récupérées dans les balises html
-  cityname.appendChild(nameElement); //on ajoute la nouvelle div à l'intérieur de la balise html prévue
+  nameElement.innerText = `Today in ${response["location"]["name"]}`;     //on injecte les données récupérées dans les balises html
+  cityname.appendChild(nameElement);                                      //on ajoute la nouvelle div à l'intérieur de la balise html prévue
 
-  lastUpdateElement.innerText = `Last update${response["current"]["last_updated"]}`;
+  lastUpdateElement.innerText = `Last update: ${response["current"]["last_updated"]}`;
   lastUpdate.appendChild(lastUpdateElement);
 
   weatherElement.innerText = `${response["current"]["condition"]["text"]}`;
@@ -73,21 +68,18 @@ const fetchWeather = async () => {
   humidityElement.innerText = `${response["current"]["humidity"]} %`;
   humidity.appendChild(humidityElement);
 
-  fetchIcons();
+  fetchIcons(); //on appelle la fonction fetchIcons, qui va filtrer et injecter les icônes dans le html
 };
 
-// Appel à l'API WeatherBit = prévisions des température min. et max.
+// Appel à l'API WeatherBit = prévisions des température min. et max. J+1
 const fetchTempForecast = async () => {
   let input = document.getElementById("inputbox").value; //on récupère la valeur de l'input (nom de ville entré par l'utilisateur)
   if (!input) {
-    input = "Paris";
-    console.log(input);
+    input = "Paris";                                    //si l'input est undefined, on met Paris
   }
-  const response = await fetch(
-    `https://api.weatherbit.io/v2.0/forecast/daily?city=${input}&days=3&key=52c8e3c84ece4734898ad2b0032941c2`
-  );
-  let data = await response.json();
-  forecastTemperatureMinElement.innerText = `${data.data[1].min_temp} °C`;
+  const response = await fetch(`https://api.weatherbit.io/v2.0/forecast/daily?city=${input}&days=3&key=52c8e3c84ece4734898ad2b0032941c2`); //appel à la deuxième API
+  let data = await response.json();   //on transforme les données au format json
+  forecastTemperatureMinElement.innerText = `${data.data[1].min_temp} °C`; //on injecte les données dans le html
   forecastTemperatureMin.appendChild(forecastTemperatureMinElement);
   forecastTemperatureMaxElement.innerText = `${data.data[1].max_temp} °C`;
   forecastTemperatureMax.appendChild(forecastTemperatureMaxElement);
@@ -97,64 +89,55 @@ const fetchTempForecast = async () => {
 const fetchWeatherForecast = async () => {
   let input = document.getElementById("inputbox").value; //on récupère la valeur de l'input (nom de ville entré par l'utilisateur)
   if (!input) {
-    input = "Paris";
-    console.log(input);
+    input = "Paris";  //si l'input est undefined, on met Paris
   }
-  let response = await fetch(
-    `http://api.weatherapi.com/v1/forecast.json?key=c9ea07bc963e4bf7849141302240412&q=${input}&days=2&aqi=no&alerts=no`
-  );
-  let data1 = await response.json();
+  let response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=c9ea07bc963e4bf7849141302240412&q=${input}&days=2&aqi=no&alerts=no`); //appel à l'API
+  let data1 = await response.json(); //on transforme les données au format json
 
-  conditionText = `${data1.forecast.forecastday[1].day.condition.text}`;
-  console.log(data1.forecast.forecastday[1].day.condition.text);
+  conditionText = `${data1.forecast.forecastday[1].day.condition.text}`; //on injecte les données dans une variable conditionText
 
-  forecastWeatherElement.innerText = conditionText;
+  forecastWeatherElement.innerText = conditionText; //on injecte les données dans le html
   forecastWeather.appendChild(forecastWeatherElement);
 
-  fetchForecastIcon(conditionText);
+  fetchForecastIcon(conditionText); //on appelle la fonction qui définit et injecte l'icône en fonction du text (sunny, cloudy, etc.)
 };
 
-const fetchForecastIcon = async (conditionText) => {
-  let requestString = `weather_conditions.json`;
-  let data2 = await fetch(requestString); //on lance l'appel à l'API
+const fetchForecastIcon = async (conditionText) => { //fonction qui filtre le text envoyé par l'API et l'icône correspondante via le fichier json
+
+  let requestString = `weather_conditions.json`; //contient les correspondantes text et icone et image de fond
+  let data2 = await fetch(requestString); //on lance l'appel à notre fichier
   let responseJson = await data2.json(); //on transforme les données au format json
 
-  console.log(conditionText);
-
-  // forEach pour injecter l'icone text forecast
+  // on compare et on injecte 
   responseJson
-    .filter((line) => {
-      console.log(line.day, conditionText);
-
-      return line.day.toLowerCase() === conditionText.toLowerCase();
+    .filter((line) => { 
+      return line.day.toLowerCase() === conditionText.toLowerCase(); //compare les deux données
     })
-    .forEach((matchedLine) => {
+    .forEach((matchedLine) => { // on injecte l'icône correspondant au text
       forecastWeatherIconElement.innerHTML = `<img src="${matchedLine.image}" alt="${matchedLine.day}">`;
       forecastWeatherIcon.appendChild(forecastWeatherIconElement);
-      console.log(forecastWeatherIconElement.innerHTML);
     });
 };
 
-// weather-condition-ICONS
-
+//fonction qui filtre le text envoyé par l'API et l'icône correspondante via le fichier json 
 const fetchIcons = async () => {
   let requestString = `weather_conditions.json`; //on injecte l'input dans la requête API
-  let data = await fetch(requestString); //on lance l'appel à l'API
+  let data = await fetch(requestString); //on lance l'appel à notre fichier
   let responseJson = await data.json(); //on transforme les données au format json
 
-  // forEach pour injecter l'icone text courant
-  responseJson
+  
+  responseJson // on compare et on injecte 
     .filter(
-      (line) => line.day === `${response["current"]["condition"]["text"]}`
+      (line) => line.day === `${response["current"]["condition"]["text"]}` //compare les deux données
     )
-    .forEach((matchedLine) => {
+    .forEach((matchedLine) => { // on injecte l'icône et l'image de fond correspondant au text
       weatherIconElement.innerHTML = `<img src="${matchedLine.image}" alt="${matchedLine.day}">`;
       weatherIcon.appendChild(weatherIconElement);
       backgroundElement.innerHTML = `<img src="${matchedLine.background}" alt="${matchedLine.day}">`;
       background.appendChild(backgroundElement);
     });
 
-  if (`${response["current"]["temp_c"]}` >= 20) {
+  if (`${response["current"]["temp_c"]}` >= 20) { //condition pour la témpérature (icones chaud ou froid)
     temperatureIconElement.innerHTML = `<img src= "icons/thermometer-warmer.svg">`; // Temperature hot
     temperatureIcon.appendChild(temperatureIconElement);
   } else {
@@ -163,16 +146,15 @@ const fetchIcons = async () => {
   }
 };
 
-function fetchAll() {
+function fetchAll() { //fonction qui lance les différents appels aux API
   fetchWeather();
   fetchWeatherForecast();
   fetchTempForecast();
 }
 
-function initTime() {
-  //fonction qui récupère la vraie heure et l'affiche à chaque minute
+function initTime() {  //fonction qui récupère la vraie heure et l'affiche à chaque minute
 
-  let date = new Date(); //on crée l'objet Date
+  let date = new Date();    //on crée l'objet Date
   hours = date.getHours(); //on récupère l'heure dans la variable hours
   minutes = date.getMinutes(); //on récupère les minutes dans la variable minutes
   seconds = date.getSeconds(); //on récupère les secondes dans la variable seconds
@@ -182,8 +164,7 @@ function initTime() {
   setTimeout(initTime, (60 - seconds) * 1000); // On re-synchronise toutes les minutes
 }
 
-function incrementSeconds() {
-  //Fonction qui incrémente les secondes et déplace le soleil
+function incrementSeconds() {  //Fonction qui incrémente les secondes
 
   seconds++; //on incremente les secondes
 
@@ -221,37 +202,18 @@ function showTime() {
   }
 
   timeElement.className = "time";
-  timeElement.innerText = `Il est ${formatHours}:${formatMinutes}:${formatSeconds}`; // on injecte les heures, minutes et secondes dans la balise
+  timeElement.innerText = `${formatHours} : ${formatMinutes} : ${formatSeconds}`; // on injecte les heures, minutes et secondes dans la balise
 
   if (timeContainer) {
     timeContainer.appendChild(timeElement); // met à jour l'élement timeElement
   }
 }
-initTime();
-setInterval(incrementSeconds, 1000);
 
-// Home Page
-// async function myHome() {
-// let input = "lyon";
-// let requestStringLyon = `http://api.weatherapi.com/v1/current.json?key=b0e73d7dbcd346fc826145808242711&q=${input}&aqi=no`; //on injecte l'input dans la requête API
+//Appels des fonctions au démarrage
 
-// let dataLyon = await fetch(requestStringLyon); //on lance l'appel à l'API
-// let responseLyon = await dataLyon.json();
-// weatherElement.innerText = `${responseLyon["current"]["condition"]["text"]}`;
-// weather.appendChild(weatherElement);
+initTime();   //fonction qui récupère la vraie heure et l'affiche à chaque minute
+setInterval(incrementSeconds, 1000); //fonction qui met à jour les secondes, minutes et heures
 
-// temperatureElement.innerText = `${responseLyon["current"]["temp_c"]} °C`;
-// temperature.appendChild(temperatureElement);
-
-// windElement.innerText = `${responseLyon["current"]["wind_kph"]} km/h`;
-// wind.appendChild(windElement);
-
-// humidityElement.innerText = `${responseLyon["current"]["humidity"]} %`;
-// humidity.appendChild(humidityElement);
-// fetchIcons();
-
-fetchAll();
-// }
-// myHome();
+fetchAll(); //on lance tous les appels aux API
 
 button.addEventListener("click", fetchAll); //on ajoute une addEventListener au bouton : au click, on lance la fonction fetchAll
